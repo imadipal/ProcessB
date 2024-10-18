2aca4d2646cb56ab20c99b5b56b0217c42b06f61
adityapal <apal@tekion.com>
2024-10-18 17:44:37 +0530

    CORS

--- src/main/java/com/knowtheprocessbackend/knowtheprocessbackend/config/WebConfig.java
+++ src/main/java/com/knowtheprocessbackend/knowtheprocessbackend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:3000") // Allow your frontend
+                .allowedOrigins("https://process-five.vercel.app") // Allow your frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials
